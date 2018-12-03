bfe7f78dc0913e412b33793599c1de673ffcebfc
Starr Horne <starr@chromahq.com>
2018-12-03 13:45:36 -0800

    Solve part 2, day 1

--- lib/day1.ex
+++ lib/day1.ex
@@ -9,7 +9,26 @@ defmodule Day1 do
 
   """
   def solve_part_1(data) do
-    data |> prepare |> transform
+    data |> prepare |> Enum.sum()
+  end
+
+  @doc ~S"""
+  Find first repeating sum
+
+  ## Examples
+
+      iex> Day1.solve_part_2(["1\n", "-1\n"])
+      0
+
+      iex> Day1.solve_part_2(~w(+3 +3 -3))
+      3
+
+      iex> Day1.solve_part_2(~w(+3 +3 +4 -2 -4))
+      10
+  """
+  def solve_part_2(data) do
+    nums = data |> prepare
+    find_repeat(nums, nums, 0, MapSet.new([0]))
   end
 
   defp prepare(data) do
@@ -20,7 +39,17 @@ defmodule Day1 do
     end
   end
 
-  defp transform(data) do
-    data |> Enum.sum()
+  defp find_repeat([], data, sum, memo) do
+    find_repeat(data, data, sum, memo)
+  end
+
+  defp find_repeat([head | tail], data, sum, memo) do
+    current = sum + head
+
+    if MapSet.member?(memo, current) do
+      current
+    else
+      find_repeat(tail, data, current, MapSet.put(memo, current))
+    end
   end
 end
--- main.ex
+++ main.ex
@@ -1,1 +1,2 @@
 IO.puts("Day 1, Part 1: #{Day1.solve_part_1(File.stream!("input.txt"))}")
+IO.puts("Day 1, Part 2: #{Day1.solve_part_2(File.stream!("input.txt"))}")
